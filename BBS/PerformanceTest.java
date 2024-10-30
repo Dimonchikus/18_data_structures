2d2bfc33beb83788ceba63db75fc05343c1555e2
dimonchikakus <d.rumsha@cidtd.mil.ua>
2024-10-30 20:27:32 +0200

    work

--- BBS/PerformanceTest.java
+++ BBS/PerformanceTest.java
@@ -9,33 +9,27 @@ public class PerformanceTest {
         int datasetSize = 10000; // Adjust as needed
         int[] dataset = new int[datasetSize];
 
-        // Generate random dataset
         for (int i = 0; i < datasetSize; i++) {
             dataset[i] = rand.nextInt(Integer.MAX_VALUE);
         }
 
-        // Arrays to hold operation times
         long[] insertTimes = new long[100];
         long[] deleteTimes = new long[100];
         long[] searchTimes = new long[100];
 
-        // Perform operations and measure time
         for (int i = 0; i < 100; i++) {
             int value = dataset[rand.nextInt(datasetSize)];
 
-            // Measure insert time
             long startInsert = System.nanoTime();
             tree.insert(value);
             long endInsert = System.nanoTime();
             insertTimes[i] = endInsert - startInsert;
 
-            // Measure search time
             long startSearch = System.nanoTime();
             boolean found = tree.contains(value);
             long endSearch = System.nanoTime();
             searchTimes[i] = endSearch - startSearch;
 
-            // Measure delete time
             long startDelete = System.nanoTime();
             tree.delete(value);
             long endDelete = System.nanoTime();
--- CountingSort/CountingSort.java
+++ CountingSort/CountingSort.java
@@ -7,7 +7,6 @@ public class CountingSort {
             return;
         }
 
-        // Find the maximum and minimum values
         int max = array[0], min = array[0];
         for (int num : array) {
             if (num > max) {
@@ -21,23 +20,19 @@ public class CountingSort {
         int range = max - min + 1;
         int[] count = new int[range];
 
-        // Count the occurrences
         for (int num : array) {
             count[num - min]++;
         }
 
-        // Modify count array
         for (int i = 1; i < count.length; i++) {
             count[i] += count[i - 1];
         }
 
-        // Sort the array
         int[] output = new int[array.length];
         for (int i = array.length - 1; i >= 0; i--) {
             output[--count[array[i] - min]] = array[i];
         }
 
-        // Copy back to the original array
         System.arraycopy(output, 0, array, 0, array.length);
     }
 }
